884d94f1993cd97058e128a838f9729dd6abb521
Juan David <gottygarcia@gmail.com>
2020-08-01 19:56:51 -0500

    Variables de entorno y globals

---
+++ conceptos/entorno.js
@@ -0,0 +1,5 @@
+const nombre = process.env.NOMBRE || 'Juan'
+const web = process.env.WEB || 'Juan.com'
+
+console.log('hola ' + nombre)
+console.log('web ' + web)
---
+++ modulos/globals.js
@@ -0,0 +1,11 @@
+console.log(global)
+
+console.log(process)
+
+console.log(__dirname)
+
+console.log(__filename)
+
+global.miVariable = 'El valor'
+
+console.log(global.miVariable)
